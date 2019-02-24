ef10d4614cf1629e1b8a35c6eaf392b5413d06d4
Creastery <Creastery@users.noreply.github.com>
2019-02-25 01:19:20 +0800

    Storage: refactor class `JsonAdaptedPerson` to `JsonAdaptedModule`
    We are in the first refactoring phase of morphing `persons`-oriented
    Address Book application to a `modules`-oriented Degree Planner
    application.
    
    In this first phase of refactoring, let's refactor class
    `JsonAdaptedPerson` to `JsonAdaptedModule` and:
    * refactor variable names containing `*person*` to `*module*`
    * cascade the changes to all affected classes

--- src/main/java/seedu/address/storage/JsonSerializableAddressBook.java
+++ src/main/java/seedu/address/storage/JsonSerializableAddressBook.java
@@ -21,13 +21,13 @@ class JsonSerializableAddressBook {
 
     public static final String MESSAGE_DUPLICATE_PERSON = "Persons list contains duplicate person(s).";
 
-    private final List<JsonAdaptedPerson> persons = new ArrayList<>();
+    private final List<JsonAdaptedModule> persons = new ArrayList<>();
 
     /**
      * Constructs a {@code JsonSerializableAddressBook} with the given persons.
      */
     @JsonCreator
-    public JsonSerializableAddressBook(@JsonProperty("persons") List<JsonAdaptedPerson> persons) {
+    public JsonSerializableAddressBook(@JsonProperty("persons") List<JsonAdaptedModule> persons) {
         this.persons.addAll(persons);
     }
 
@@ -37,7 +37,7 @@ class JsonSerializableAddressBook {
      * @param source future changes to this will not affect the created {@code JsonSerializableAddressBook}.
      */
     public JsonSerializableAddressBook(ReadOnlyAddressBook source) {
-        persons.addAll(source.getModuleList().stream().map(JsonAdaptedPerson::new).collect(Collectors.toList()));
+        persons.addAll(source.getModuleList().stream().map(JsonAdaptedModule::new).collect(Collectors.toList()));
     }
 
     /**
@@ -47,8 +47,8 @@ class JsonSerializableAddressBook {
      */
     public AddressBook toModelType() throws IllegalValueException {
         AddressBook addressBook = new AddressBook();
-        for (JsonAdaptedPerson jsonAdaptedPerson : persons) {
-            Module module = jsonAdaptedPerson.toModelType();
+        for (JsonAdaptedModule jsonAdaptedModule : persons) {
+            Module module = jsonAdaptedModule.toModelType();
             if (addressBook.hasModule(module)) {
                 throw new IllegalValueException(MESSAGE_DUPLICATE_PERSON);
             }
