8ef9aaff7042a9e9ed649adbf8f35262e9ab006a
Creastery <Creastery@users.noreply.github.com>
2019-02-26 13:48:20 +0800

    test/data: refactor `JsonSerializableAddressBookTest` JSON files
    We are in the second refactoring phase of morphing `persons`-oriented
    Address Book application to a `modules`-oriented Degree Planner
    application.
    
    In this second phase of refactoring, let's refactor the JSON test data
    files for `JsonSerializableAddressBookTest` by:
    * refactoring JSON files to rename as `*module*` instead of `*person*`
    * replacing `*person*` with `*module*` in the file contents

--- src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
@@ -17,9 +17,9 @@ import seedu.address.testutil.TypicalModules;
 public class JsonSerializableAddressBookTest {
 
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableAddressBookTest");
-    private static final Path TYPICAL_MODULES_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsAddressBook.json");
-    private static final Path INVALID_MODULE_FILE = TEST_DATA_FOLDER.resolve("invalidPersonAddressBook.json");
-    private static final Path DUPLICATE_MODULE_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonAddressBook.json");
+    private static final Path TYPICAL_MODULES_FILE = TEST_DATA_FOLDER.resolve("typicalModulesAddressBook.json");
+    private static final Path INVALID_MODULE_FILE = TEST_DATA_FOLDER.resolve("invalidModuleAddressBook.json");
+    private static final Path DUPLICATE_MODULE_FILE = TEST_DATA_FOLDER.resolve("duplicateModuleAddressBook.json");
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
