6e948b98d7ea990f11cbd76953ca7a4cf4d490c9
lycjackie <leeyc@comp.nus.edu.sg>
2019-03-02 02:35:35 +0800

    test/parser: update `AddressBookParserTest` broken unit test
    We have update the `FindCommand` to require `PREFIX_NAME` to identify
    the keywords it is finding for.
    
    However, this caused the unit test in `AddressBookParserTest` class
    to fail.
    
    The broken unit test is
    * parseCommand_find()
    
    Let's fix the faulty unit tests by
    * having the `PREFIX_NAME` appended before the`keyword`

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_MODULE;
 
 import java.util.Arrays;
@@ -79,7 +80,7 @@ public class AddressBookParserTest {
     public void parseCommand_find() throws Exception {
         List<String> keywords = Arrays.asList("foo", "bar", "baz");
         FindCommand command = (FindCommand) parser.parseCommand(
-                FindCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")));
+                FindCommand.COMMAND_WORD + " " + PREFIX_NAME + keywords.stream().collect(Collectors.joining(" ")));
         assertEquals(new FindCommand(new NameContainsKeywordsPredicate(keywords)), command);
     }
 
