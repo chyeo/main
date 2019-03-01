3d2810020e9d2ed529a3b6e3e9c376f0a36954e3
lycjackie <leeyc@comp.nus.edu.sg>
2019-03-01 15:51:27 +0800

    Logic: make `FindCommandParser#parse(...)` parse `PREFIX_NAME`
    With the plan to include more attributes keyword into the `FindCommand`
    
    As such, we need to be able to identify which attribute the keyword
    is referring to.
    
    Let's update `FindCommandParser#parse` to accept `PREFIX_NAME`
    
    Disclaimer: This commit will break all existing unit tests, and will be fixed later on

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,11 +1,14 @@
 package seedu.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 import java.util.Arrays;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.module.KeywordsPredicate;
 import seedu.address.model.module.NameContainsKeywordsPredicate;
 
 /**
@@ -16,18 +19,27 @@ public class FindCommandParser implements Parser<FindCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns an FindCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
+        requireNonNull(args);
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
-
-        String[] nameKeywords = trimmedArgs.split("\\s+");
-
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME);
+        KeywordsPredicate predicate = null;
+        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
+            String[] nameKeywords = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get())
+                    .toString().split("\\s+");
+            predicate = new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords));
+        } else {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+        return new FindCommand(predicate);
     }
-
 }
