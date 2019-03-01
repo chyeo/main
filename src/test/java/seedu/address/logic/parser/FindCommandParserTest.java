ea05da395f45d6f5a43041dbbfb869608bcde976
lycjackie <leeyc@comp.nus.edu.sg>
2019-03-02 01:10:03 +0800

    test/parser: add unit test to `FindCommandParserTest`
    We have update the `parser` to take in `PREFIX_CREDITS`
    
    Let's add test case to ensure that the behavior of`FindCommandParser`
    is what we expect

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -2,6 +2,7 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_CODE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CREDITS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
@@ -12,6 +13,7 @@ import org.junit.Test;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.model.module.CodeContainsKeywordsPredicate;
+import seedu.address.model.module.CreditsContainsKeywordsPredicate;
 import seedu.address.model.module.NameContainsKeywordsPredicate;
 
 public class FindCommandParserTest {
@@ -35,6 +37,12 @@ public class FindCommandParserTest {
         FindCommand expectedFindCodeCommand =
                 new FindCommand(new CodeContainsKeywordsPredicate(Arrays.asList("CS1231", "GET1004")));
         assertParseSuccess(parser, "find " + PREFIX_CODE + "CS1231 GET1004", expectedFindCodeCommand);
+
+        /* TODO: Due to the regex `Credits` have now, the test case does not reflect an actual Module Credits
+         This will be update after proper regex is done. */
+        FindCommand expectedFindCreditsCommand =
+                new FindCommand(new CreditsContainsKeywordsPredicate(Arrays.asList("999")));
+        assertParseSuccess(parser, "find " + PREFIX_CREDITS + "999", expectedFindCreditsCommand);
     }
 
 }
