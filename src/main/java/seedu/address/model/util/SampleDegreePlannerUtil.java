f8087240c5a58020571596298fa7f7bc80b3fb68
truegitnovice <e0191784@u.nus.edu>
2019-03-03 02:20:46 +0800

    Model: update to provide sample degreePlanners

--- src/main/java/seedu/address/model/util/SampleDegreePlannerUtil.java
+++ src/main/java/seedu/address/model/util/SampleDegreePlannerUtil.java
@@ -1,4 +1,34 @@
 package seedu.address.model.util;
 
+import seedu.address.model.DegreePlannerList;
+import seedu.address.model.ReadOnlyDegreePlannerList;
+import seedu.address.model.module.Code;
+import seedu.address.model.planner.DegreePlanner;
+import seedu.address.model.planner.DegreePlannerYear;
+import seedu.address.model.planner.DegreePlannerSemester;
+
+/**
+ * Contains utility methods for populating {@code AddressBook} with sample data.
+ */
 public class SampleDegreePlannerUtil {
+    public static DegreePlanner[] getSampleDegreePlanners() {
+        return new DegreePlanner[] {
+                new DegreePlanner(new Code("CS1010"), new DegreePlannerYear("1"),
+                        new DegreePlannerSemester("1")),
+                new DegreePlanner(new Code("CS1231"), new DegreePlannerYear("1"),
+                        new DegreePlannerSemester("1")),
+                new DegreePlanner(new Code("MA1101R"), new DegreePlannerYear("1"),
+                        new DegreePlannerSemester("1")),
+                new DegreePlanner(new Code("GER1000"), new DegreePlannerYear("1"),
+                        new DegreePlannerSemester("1")),
+        };
+    }
+
+    public static ReadOnlyDegreePlannerList getSampleDegreePlannerList() {
+        DegreePlannerList sampleDegreePlannerList = new DegreePlannerList();
+        for (DegreePlanner sampleDegreePlanner : getSampleDegreePlanners()) {
+            sampleDegreePlannerList.addDegreePlanner(sampleDegreePlanner);
+        }
+        return sampleDegreePlannerList;
+    }
 }
