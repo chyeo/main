14f6a80e9de89775b14b606c35ee384ff2cc21ea
truegitnovice <e0191784@u.nus.edu>
2019-03-02 19:52:19 +0800

    Planner: add exceptions

---
+++ src/main/java/seedu/address/model/planner/exceptions/DegreePlannerNotFoundException.java
@@ -0,0 +1,7 @@
+package seedu.address.model.planner.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified planner.
+ */
+public class DegreePlannerNotFoundException extends RuntimeException {
+}
---
+++ src/main/java/seedu/address/model/planner/exceptions/DuplicateDegreePlannerException.java
@@ -0,0 +1,12 @@
+package seedu.address.model.planner.exceptions;
+
+/**
+ * Signals that the operation will result in duplicate Planners (Planners are considered duplicates if they have the
+ * same
+ * identity).
+ */
+public class DuplicateDegreePlannerException extends RuntimeException {
+    public DuplicateDegreePlannerException() {
+        super("Operation would result in duplicate planners");
+    }
+}
