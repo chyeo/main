4a8bf0b5271d93303056c87ec7afaec7e0534ccb
truegitnovice <e0191784@u.nus.edu>
2019-03-03 11:22:09 +0800

    Storage: update to handle modified DegreePlannerList

--- src/main/java/seedu/address/storage/JsonAdaptedDegreePlannerList.java
+++ src/main/java/seedu/address/storage/JsonAdaptedDegreePlannerList.java
@@ -1,5 +1,11 @@
 package seedu.address.storage;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -7,8 +13,8 @@ import seedu.address.commons.exceptions.IllegalValueException;
 
 import seedu.address.model.module.Code;
 import seedu.address.model.planner.DegreePlanner;
-import seedu.address.model.planner.DegreePlannerSemester;
-import seedu.address.model.planner.DegreePlannerYear;
+import seedu.address.model.planner.Semester;
+import seedu.address.model.planner.Year;
 
 /**
  * Jackson-friendly version of {@link DegreePlanner}.
@@ -17,28 +23,30 @@ public class JsonAdaptedDegreePlannerList {
 
     public static final String MISSING_FIELD_MESSAGE_FORMAT = "degreePlanner's %s field is missing!";
 
-    private final String code;
     private final String year;
     private final String semester;
+    private final List<JsonAdaptedCode> codes = new ArrayList<>();
 
     /**
      * Constructs a {@code JsonAdaptedModule} with the given module details.
      */
     @JsonCreator
-    public JsonAdaptedDegreePlannerList(@JsonProperty("code") String code, @JsonProperty("year") String year,
-            @JsonProperty("semester") String semester) {
-        this.code = code;
+    public JsonAdaptedDegreePlannerList(@JsonProperty("year") String year,
+            @JsonProperty("semester") String semester, @JsonProperty("codes") List<JsonAdaptedCode> codes) {
         this.year = year;
         this.semester = semester;
+        if (codes != null) {
+            this.codes.addAll(codes);
+        }
     }
 
     /**
      * Converts a given {@code DegreePlanner} into this class for Jackson use.
      */
     public JsonAdaptedDegreePlannerList(DegreePlanner source) {
-        code = source.getCode().value;
-        year = source.getYear().plannerYear;
+        year = source.getYear().year;
         semester = source.getSemester().plannerSemester;
+        codes.addAll(source.getCodes().stream().map(JsonAdaptedCode::new).collect(Collectors.toList()));
     }
 
     /**
@@ -47,32 +55,31 @@ public class JsonAdaptedDegreePlannerList {
      * @throws IllegalValueException if there were any data constraints violated in the adapted degreePlanner.
      */
     public DegreePlanner toModelType() throws IllegalValueException {
-        if (code == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Code.class.getSimpleName()));
+        final List<Code> plannerCodes = new ArrayList<>();
+        for (JsonAdaptedCode code : codes) {
+            plannerCodes.add(code.toModelType());
         }
-        if (!Code.isValidCode(code)) {
-            throw new IllegalValueException(Code.MESSAGE_CONSTRAINTS);
-        }
-        final Code modelCode = new Code(code);
 
         if (year == null) {
             throw new IllegalValueException(
-                    String.format(MISSING_FIELD_MESSAGE_FORMAT, DegreePlannerYear.class.getSimpleName()));
+                    String.format(MISSING_FIELD_MESSAGE_FORMAT, Year.class.getSimpleName()));
         }
-        if (!DegreePlannerYear.isValidYear(year)) {
-            throw new IllegalValueException(DegreePlannerYear.MESSAGE_YEAR_CONSTRAINTS);
+        if (!Year.isValidYear(year)) {
+            throw new IllegalValueException(Year.MESSAGE_YEAR_CONSTRAINTS);
         }
-        final DegreePlannerYear modelYear = new DegreePlannerYear(year);
+        final Year modelYear = new Year(year);
 
         if (semester == null) {
             throw new IllegalValueException(
-                    String.format(MISSING_FIELD_MESSAGE_FORMAT, DegreePlannerSemester.class.getSimpleName()));
+                    String.format(MISSING_FIELD_MESSAGE_FORMAT, Semester.class.getSimpleName()));
         }
-        if (!DegreePlannerSemester.isValidSemester(semester)) {
-            throw new IllegalValueException(DegreePlannerSemester.MESSAGE_SEMESTER_CONSTRAINTS);
+        if (!Semester.isValidSemester(semester)) {
+            throw new IllegalValueException(Semester.MESSAGE_SEMESTER_CONSTRAINTS);
         }
-        final DegreePlannerSemester modelSemester = new DegreePlannerSemester(semester);
+        final Semester modelSemester = new Semester(semester);
 
-        return new DegreePlanner(modelCode, modelYear, modelSemester);
+        final Set<Code> modelCodes = new HashSet<>(plannerCodes);
+
+        return new DegreePlanner(modelYear, modelSemester, modelCodes);
     }
 }
