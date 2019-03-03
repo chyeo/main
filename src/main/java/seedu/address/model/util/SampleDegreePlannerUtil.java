e5e0b4e103a5e7c2b1dacacf04f2491df832e461
truegitnovice <e0191784@u.nus.edu>
2019-03-03 15:42:47 +0800

    Model: update SampleDegreePlanner

--- src/main/java/seedu/address/model/util/SampleDegreePlannerUtil.java
+++ src/main/java/seedu/address/model/util/SampleDegreePlannerUtil.java
@@ -18,21 +18,21 @@ public class SampleDegreePlannerUtil {
     public static DegreePlanner[] getSampleDegreePlanners() {
         return new DegreePlanner[] {
             new DegreePlanner(new Year("1"), new Semester("1"),
-                    getCodeSet("CS1010", "CS1231", "MA1101R", "GER1000", "EC1301")),
+                    getCodeSet()),
             new DegreePlanner(new Year("1"), new Semester("2"),
-                    getCodeSet("CS2100", "CS2102", "CS2040C", "CS2107", "GEQ1000")),
+                    getCodeSet()),
             new DegreePlanner(new Year("2"), new Semester("1"),
-                    getCodeSet("CS2105", "GEH1023", "CS2106", "IS1103", "GET1023")),
+                    getCodeSet()),
             new DegreePlanner(new Year("2"), new Semester("2"),
-                    getCodeSet("CS2113T", "CS2101", "ST2334", "MA1521", "CS3235")),
+                    getCodeSet()),
             new DegreePlanner(new Year("3"), new Semester("1"),
-                    getCodeSet("CS3233", "IS3103", "CS4236", "CS4238", "CS3210")),
+                    getCodeSet()),
             new DegreePlanner(new Year("3"), new Semester("2"),
-                    getCodeSet("CS3243", "CS3244", "GEH1068", "LAK1201", "CS5231")),
+                    getCodeSet()),
             new DegreePlanner(new Year("4"), new Semester("1"),
-                    getCodeSet("CS5321", "CS5331", "EC3303", "IFS4205")),
+                    getCodeSet()),
             new DegreePlanner(new Year("4"), new Semester("2"),
-                    getCodeSet("CS5332", "IFS4102", "IS4302", "IS4234", "EC3304"))
+                    getCodeSet())
         };
     }
 
