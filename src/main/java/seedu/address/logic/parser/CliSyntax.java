e1b282098e0fb8ccea1cdb41492df8caf86214ec
Creastery <Creastery@users.noreply.github.com>
2019-03-03 18:50:24 +0800

    Logic/parser: update prefixes in `CliSyntax`
    After the initial refactoring, the command prefixes were left unchanged.
    
    The prefixes do not match the name of the `CliSyntax_PREFIX_`-constants
    anymore.
    
    Let's update the prefix values in `CliSyntax` respectively.

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -6,9 +6,9 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_CREDITS = new Prefix("p/");
-    public static final Prefix PREFIX_CODE = new Prefix("a/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_NAME = new Prefix("name/");
+    public static final Prefix PREFIX_CODE = new Prefix("code/");
+    public static final Prefix PREFIX_CREDITS = new Prefix("credits/");
+    public static final Prefix PREFIX_TAG = new Prefix("tag/");
 
 }
