b8404b8e85a7a7da2311f5b15116378e68398dd4
lycjackie <leeyc@comp.nus.edu.sg>
2019-03-15 12:14:28 +0800

    test/parser: add unit tests to `FindCommandParserTest`
    We have update the `parser` logic to be able to parse strings into
    multiple objects. (i.e. `List<Credits>`)
    
    Let's add unit tests to ensure that the behaviour of `FindCommandParser`
    is what we expect and:
    - remove TODO as it was fix by previous commit

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CODE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_CREDITS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
@@ -11,6 +12,7 @@ import java.util.Arrays;
 import org.junit.Test;
 
 import seedu.address.logic.commands.FindCommand;
+import seedu.address.model.module.CodeContainsKeywordsPredicate;
 import seedu.address.model.module.CreditsContainsKeywordsPredicate;
 import seedu.address.model.module.NameContainsKeywordsPredicate;
 
@@ -21,29 +23,57 @@ public class FindCommandParserTest {
     @Test
     public void parse_emptyArg_throwsParseException() {
         assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        // No name argument -> assertFailure
+        assertParseFailure(parser, PREFIX_NAME + "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        // No code argument -> assertFailure
+        assertParseFailure(parser, PREFIX_CODE + "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        // No credits argument -> assertFailure
+        assertParseFailure(parser, PREFIX_CREDITS + "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_validArgs_returnsFindCommand() {
         // no leading and trailing whitespaces
         FindCommand expectedFindNameCommand =
+                new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice")));
+        FindCommand expectedFindMultipleNameCommand =
                 new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
-        assertParseSuccess(parser, "find " + PREFIX_NAME + "Alice Bob", expectedFindNameCommand);
+        // single keyword
+        assertParseSuccess(parser, "find " + PREFIX_NAME + "Alice", expectedFindNameCommand);
+        // multiple keywords
+        assertParseSuccess(parser, "find " + PREFIX_NAME + "Alice Bob", expectedFindMultipleNameCommand);
         // multiple whitespaces between keywords
-        assertParseSuccess(parser, "find " + PREFIX_NAME + "  Alice       Bob     ", expectedFindNameCommand);
+        assertParseSuccess(parser, "find " + PREFIX_NAME + "  Alice       Bob     ", expectedFindMultipleNameCommand);
 
-        // TODO: @lycjackie - Fix bug in FindCommandParser that breaks unit test
-        /*
         FindCommand expectedFindCodeCommand =
+                new FindCommand(new CodeContainsKeywordsPredicate(Arrays.asList("CS1231")));
+        FindCommand expectedFindMultipleCodeCommand =
                 new FindCommand(new CodeContainsKeywordsPredicate(Arrays.asList("CS1231", "GET1004")));
-        assertParseSuccess(parser, "find " + PREFIX_CODE + "CS1231 GET1004", expectedFindCodeCommand);
-        */
+        // single keyword
+        assertParseSuccess(parser, "find " + PREFIX_CODE + "CS1231", expectedFindCodeCommand);
+        // multiple keywords
+        assertParseSuccess(parser, "find " + PREFIX_CODE + "CS1231 GET1004", expectedFindMultipleCodeCommand);
+        // multiple whitespaces keywords
+        assertParseSuccess(parser, "find " + PREFIX_CODE + "          CS1231           GET1004          ",
+                expectedFindMultipleCodeCommand);
 
         /* TODO: Due to the regex `Credits` have now, the test case does not reflect an actual Module Credits
          This will be update after proper regex is done. */
         FindCommand expectedFindCreditsCommand =
                 new FindCommand(new CreditsContainsKeywordsPredicate(Arrays.asList("999")));
+        FindCommand expectedFindMultipleCreditsCommand =
+                new FindCommand(new CreditsContainsKeywordsPredicate(Arrays.asList("999", "004", "012")));
+        // single keyword
         assertParseSuccess(parser, "find " + PREFIX_CREDITS + "999", expectedFindCreditsCommand);
+        // multiple keywords
+        assertParseSuccess(parser, "find " + PREFIX_CREDITS + "999 004 012",
+                expectedFindMultipleCreditsCommand);
+        // multiple whitespaces keywords
+        assertParseSuccess(parser, "find " + PREFIX_CREDITS + "        999           004        012",
+                expectedFindMultipleCreditsCommand);
     }
 
 }
