7b4db77da210e58a77e9a31f14493423377e746c
truegitnovice <e0191784@u.nus.edu>
2019-03-21 19:26:27 +0800

    Logic: update 'PlannerListAllCommand' to suit to new model structure
    'PlannListAllCommand' is updated to suit the new model which
    combines 'VersionedDegreePlannerList' into 'VersionedAddressBook'

--- src/main/java/seedu/address/logic/commands/PlannerListAllCommand.java
+++ src/main/java/seedu/address/logic/commands/PlannerListAllCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_DEGREE_PLANNERS;
 
 import java.util.stream.Collectors;
 
@@ -21,6 +22,7 @@ public class PlannerListAllCommand extends Command {
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
+        model.updateFilteredDegreePlannerList(PREDICATE_SHOW_ALL_DEGREE_PLANNERS);
         StringBuilder degreePlannerListContent = new StringBuilder();
         for (DegreePlanner degreePlanner : model.getFilteredDegreePlannerList()) {
             degreePlannerListContent
