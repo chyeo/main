297fa2eeddf6231403adb597ad11b72e427e9beb
truegitnovice <e0191784@u.nus.edu>
2019-03-29 22:13:09 +0800

    Test: update to include test for 'planner_move' command
    In order to implement 'planner_move' in correct manner, test cases
    are needed to carry out checks.
    
    To allow more variety of checks to be carried out
    typicalDegreePlannerAddressBook needs to be modified to exclude
    Year 4 Semester 1 and Year 4 Semester 2 degree planners.
    
    Let's add variety of test cases for 'planner_move' and let methods
    created to help carry out 'planner_move' to be included in test.
    
    Let's also remove Year 4 degree planners mentioned to support more
    test cases for 'planner_move'.

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -4,7 +4,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CODE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SEMESTER;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_YEAR;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_MODULE;
 
 import java.util.Arrays;
@@ -25,12 +28,16 @@ import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.PlannerListAllCommand;
+import seedu.address.logic.commands.PlannerMoveCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.module.Code;
 import seedu.address.model.module.Module;
 import seedu.address.model.module.NameContainsKeywordsPredicate;
+import seedu.address.model.planner.Semester;
+import seedu.address.model.planner.Year;
 import seedu.address.testutil.EditModuleDescriptorBuilder;
 import seedu.address.testutil.ModuleBuilder;
 import seedu.address.testutil.ModuleUtil;
@@ -120,6 +127,14 @@ public class AddressBookParserTest {
     public void parseCommand_plannerListAll() throws Exception {
         assertTrue(parser.parseCommand(PlannerListAllCommand.COMMAND_WORD) instanceof PlannerListAllCommand);
         assertTrue(parser.parseCommand(PlannerListAllCommand.COMMAND_WORD + " 3") instanceof PlannerListAllCommand);
+    }
+
+    @Test
+    public void parseCommand_plannerMove() throws Exception {
+        PlannerMoveCommand command = (PlannerMoveCommand) parser.parseCommand(
+                PlannerMoveCommand.COMMAND_WORD + " " + PREFIX_YEAR + "1 " + PREFIX_SEMESTER + "2 "
+                        + PREFIX_CODE + "CS1010");
+        assertEquals(new PlannerMoveCommand(new Year("1"), new Semester("2"), new Code("CS1010")), command);
     }
 
     @Test
