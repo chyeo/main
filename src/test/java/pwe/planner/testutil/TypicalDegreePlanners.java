00b0d09dd2437b8d1b1f819c271a77dbcd79fb4d
truegitnovice <e0191784@u.nus.edu>
2019-04-06 21:25:23 +0800

    TestUtil: update to allow using variables in other classes
    Currently 'TypicalDegreePlanners' does not allow utilizing of its
    variables (e.g. YEAR_1_SEMESTER_1) as these variables are 'private'.
    
    Let's update these variables from 'private' to 'public' so that
    it can be used in test cases for other classes such as 'planner_show'.

--- src/test/java/pwe/planner/testutil/TypicalDegreePlanners.java
+++ src/test/java/pwe/planner/testutil/TypicalDegreePlanners.java
@@ -13,19 +13,36 @@ import pwe.planner.model.planner.DegreePlanner;
  */
 public class TypicalDegreePlanners {
 
-    private static final DegreePlanner YEAR_1_SEMESTER_1 =
-            new DegreePlannerBuilder().withYear("1").withSemester("1")
-                    .withCodes("CS1231", "CS2100", "CS1010", "CS2040C", "CS2102").build();
-    private static final DegreePlanner YEAR_1_SEMESTER_2 =
-            new DegreePlannerBuilder().withYear("1").withSemester("2").build();
-    private static final DegreePlanner YEAR_2_SEMESTER_1 =
-            new DegreePlannerBuilder().withYear("2").withSemester("1").build();
-    private static final DegreePlanner YEAR_2_SEMESTER_2 =
-            new DegreePlannerBuilder().withYear("2").withSemester("2").build();
-    private static final DegreePlanner YEAR_3_SEMESTER_1 =
-            new DegreePlannerBuilder().withYear("3").withSemester("1").build();
-    private static final DegreePlanner YEAR_3_SEMESTER_2 =
-            new DegreePlannerBuilder().withYear("3").withSemester("2").build();
+    public static final DegreePlanner YEAR_1_SEMESTER_1 = new DegreePlannerBuilder()
+            .withYear("1")
+            .withSemester("1")
+            .withCodes("CS1231", "CS2100", "CS1010", "CS2040C", "CS2102")
+            .build();
+
+    public static final DegreePlanner YEAR_1_SEMESTER_2 = new DegreePlannerBuilder()
+            .withYear("1")
+            .withSemester("2")
+            .build();
+
+    public static final DegreePlanner YEAR_2_SEMESTER_1 = new DegreePlannerBuilder()
+            .withYear("2")
+            .withSemester("1")
+            .build();
+
+    public static final DegreePlanner YEAR_2_SEMESTER_2 = new DegreePlannerBuilder()
+            .withYear("2")
+            .withSemester("2")
+            .build();
+
+    public static final DegreePlanner YEAR_3_SEMESTER_1 = new DegreePlannerBuilder()
+            .withYear("3")
+            .withSemester("1")
+            .build();
+
+    public static final DegreePlanner YEAR_3_SEMESTER_2 = new DegreePlannerBuilder()
+            .withYear("3")
+            .withSemester("2")
+            .build();
 
     /**
      * Returns an {@code Application} with all the typical degree planners.
