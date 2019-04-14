c5fcb999b2926051adae812e02a83a159ed795a2
Ngo Wei Lin <Creastery@users.noreply.github.com>
2019-04-14 15:23:49 +0800

    Model/util: update `SampleDataUtil` (#146)
    Let's update the `SampleDataUtil` to populate our application with more
    sample data to facilitate testing, and reduce time spent by users to
    create their customised degree plan using our application.

--- src/main/java/pwe/planner/model/util/SampleDataUtil.java
+++ src/main/java/pwe/planner/model/util/SampleDataUtil.java
@@ -22,6 +22,7 @@ import pwe.planner.model.tag.Tag;
  * Contains utility methods for populating {@code Application} with sample data.
  */
 public class SampleDataUtil {
+    // Sample modules that can be used
     private static final Module CS1010 = new Module(
             new Code("CS1010"),
             new Name("Programming Methodology"),
@@ -46,7 +47,7 @@ public class SampleDataUtil {
             new Credits("4"),
             getSemesterSet("1", "2"),
             getCorequisiteSet(),
-            getTagSet("linkedlist", "stack", "queue", "hashtable", "heap", "avltree", "graph", "sssp")
+            getTagSet("algorithm", "linkedlist", "stack", "queue", "hashtable", "heap", "avltree", "graph", "sssp")
     );
 
     private static final Module CS2100 = new Module(
@@ -58,25 +59,237 @@ public class SampleDataUtil {
             getTagSet("boolean", "mips", "assembly", "circuit", "flipflop", "pipelining", "cache")
     );
 
+    private static final Module CS2101 = new Module(
+            new Code("CS2101"),
+            new Name("Effective Communication for Computing Professionals"),
+            new Credits("4"),
+            getSemesterSet("1", "2"),
+            getCorequisiteSet("CS2113T"),
+            getTagSet("communication", "presentation", "negotiation", "documentation", "reflection")
+    );
+
     private static final Module CS2102 = new Module(
             new Code("CS2102"),
             new Name("Database Systems"),
             new Credits("4"),
-            getSemesterSet("1", "2"),
+            getSemesterSet("1", "2", "4"),
             getCorequisiteSet(),
             getTagSet("database", "rdbms", "entity", "sql", "normalisation")
     );
 
+    private static final Module CS2105 = new Module(
+            new Code("CS2105"),
+            new Name("Introduction to Computer Networks"),
+            new Credits("4"),
+            getSemesterSet("1", "2"),
+            getCorequisiteSet(),
+            getTagSet("network", "tcp", "ip", "udp", "rdt")
+    );
+
+    private static final Module CS2106 = new Module(
+            new Code("CS2106"),
+            new Name("Introduction to Operating Systems"),
+            new Credits("4"),
+            getSemesterSet("1", "2"),
+            getCorequisiteSet(),
+            getTagSet("os", "syscall", "process", "scheduling", "memory")
+    );
+
+    private static final Module CS2107 = new Module(
+            new Code("CS2107"),
+            new Name("Introduction to Information Security"),
+            new Credits("4"),
+            getSemesterSet("1", "2"),
+            getCorequisiteSet(),
+            getTagSet("infosec", "crypto", "law")
+    );
+
+    private static final Module CS2113T = new Module(
+            new Code("CS2113T"),
+            new Name("Software Engineering and Object-Oriented Programming"),
+            new Credits("4"),
+            getSemesterSet("1", "2"),
+            getCorequisiteSet("CS2101"),
+            getTagSet("oop", "rcs", "uml", "junit", "design", "architecture")
+    );
+
+    private static final Module CS3235 = new Module(
+            new Code("CS3235"),
+            new Name("Introduction to Computer Security"),
+            new Credits("4"),
+            getSemesterSet("1", "2"),
+            getCorequisiteSet(),
+            getTagSet("crypto", "appsec", "memory", "overflow", "sandbox", "netsec", "websec")
+    );
+
+    private static final Module CS4238 = new Module(
+            new Code("CS4238"),
+            new Name("Computer Security Practices"),
+            new Credits("4"),
+            getSemesterSet("1", "2"),
+            getCorequisiteSet(),
+            getTagSet("appsec", "binary", "exploit", "memory", "overflow", "netsec", "websec")
+    );
+
+    private static final Module CS5331 = new Module(
+            new Code("CS5331"),
+            new Name("Web Security"),
+            new Credits("4"),
+            getSemesterSet("1", "2"),
+            getCorequisiteSet(),
+            getTagSet("websec", "xss", "sqli", "clickjacking", "sop", "cors", "oauth")
+    );
+
+    private static final Module GER1000 = new Module(
+            new Code("GER1000"),
+            new Name("Quantitative Reasoning"),
+            new Credits("4"),
+            getSemesterSet("1", "2"),
+            getCorequisiteSet(),
+            getTagSet("quantitative", "association", "measurement", "risk", "rate", "probability")
+    );
+
+    private static final Module GEQ1000 = new Module(
+            new Code("GEQ1000"),
+            new Name("Asking Questions"),
+            new Credits("4"),
+            getSemesterSet("1", "2"),
+            getCorequisiteSet(),
+            getTagSet("thinking", "philosophy", "design", "engineering", "science", "economics")
+    );
+
+    private static final Module IFS4205 = new Module(
+            new Code("IFS4205"),
+            new Name("Information Security Capstone Project"),
+            new Credits("8"),
+            getSemesterSet("1"),
+            getCorequisiteSet(),
+            getTagSet("capstone", "infosec", "project", "design", "systems")
+    );
+
+    private static final Module IS1103 = new Module(
+            new Code("IS1103"),
+            new Name("IS Innovations in Organisations and Society"),
+            new Credits("4"),
+            getSemesterSet("1"),
+            getCorequisiteSet(),
+            getTagSet("law", "ethics", "professionalism", "decision", "social", "privacy")
+    );
+
+    private static final Module IS3103 = new Module(
+            new Code("IS3103"),
+            new Name("Information Systems Leadership and Communication"),
+            new Credits("4"),
+            getSemesterSet("1", "2"),
+            getCorequisiteSet(),
+            getTagSet("business", "leadership", "communication", "interview")
+    );
+
+    private static final Module IS4231 = new Module(
+            new Code("IS4231"),
+            new Name("Information Security Management"),
+            new Credits("4"),
+            getSemesterSet("1"),
+            getCorequisiteSet(),
+            getTagSet("infosec", "business", "policies", "continuity", "risk", "management")
+    );
+
+    private static final Module MA1301 = new Module(
+            new Code("MA1301"),
+            new Name("Introductory Mathematics"),
+            new Credits("4"),
+            getSemesterSet("1", "2"),
+            getCorequisiteSet(),
+            getTagSet("sequence", "series", "functions", "derivatives", "integrals", "vectors")
+    );
+
+    private static final Module MA1101R = new Module(
+            new Code("MA1101R"),
+            new Name("Linear Algebra I"),
+            new Credits("4"),
+            getSemesterSet("1", "2"),
+            getCorequisiteSet(),
+            getTagSet("proving", "vectors", "matrices", "determinants", "euclidean")
+    );
+
+    private static final Module MA1521 = new Module(
+            new Code("MA1521"),
+            new Name("Calculus for Computing"),
+            new Credits("4"),
+            getSemesterSet("1", "2"),
+            getCorequisiteSet(),
+            getTagSet("calculus", "derivatives", "integrals", "sequence", "series")
+    );
+
+    private static final Module ST2334 = new Module(
+            new Code("ST2334"),
+            new Name("Probability and Statistics"),
+            new Credits("4"),
+            getSemesterSet("1", "2"),
+            getCorequisiteSet(),
+            getTagSet("probability", "statistics", "variance", "distribution", "hypothesis")
+    );
+
+    // Sample requirement categories that can be used
+    private static final RequirementCategory COMPUTING_FOUNDATION = new RequirementCategory(
+            new Name("Computing Foundation"),
+            new Credits("36"),
+            getCodeSet("CS1010", "CS1231", "CS2040C", "CS2100", "CS2102", "CS2105", "CS2106", "CS2113T", "IS3103")
+    );
+
+    private static final RequirementCategory INFORMATION_SECURITY_REQUIREMENTS = new RequirementCategory(
+            new Name("Information Security Requirements"),
+            new Credits("20"),
+            getCodeSet("CS2107", "CS3235", "IFS4205", "IS4231")
+    );
+
+    private static final RequirementCategory INFORMATION_SECURITY_ELECTIVES = new RequirementCategory(
+            new Name("Information Security Electives"),
+            new Credits("12"),
+            getCodeSet("CS4238", "CS5331")
+    );
+
+    private static final RequirementCategory COMPUTING_BREADTH = new RequirementCategory(
+            new Name("Computing Breadth"),
+            new Credits("20"),
+            getCodeSet()
+    );
+
+    private static final RequirementCategory IT_PROFESSIONALISM = new RequirementCategory(
+            new Name("IT Professionalism"),
+            new Credits("8"),
+            getCodeSet("IS1103", "CS2101")
+    );
+
+    private static final RequirementCategory MATHEMATICS = new RequirementCategory(
+            new Name("Mathematics"),
+            new Credits("12"),
+            getCodeSet("MA1101R", "MA1521", "ST2334")
+    );
+
+    private static final RequirementCategory GENERAL_EDUCATION = new RequirementCategory(
+            new Name("General Education"),
+            new Credits("20"),
+            getCodeSet("GER1000", "GEQ1000")
+    );
+
+    private static final RequirementCategory UNRESTRICTED_ELECTIVES = new RequirementCategory(
+            new Name("Unrestricted Electives"),
+            new Credits("32"),
+            getCodeSet("MA1301")
+    );
+
+    // Sample degree planners that can be used
     private static final DegreePlanner YEAR_1_SEMESTER_1 = new DegreePlanner(
             new Year("1"),
             new Semester("1"),
-            getCodeSet("CS1010", "CS1231", "CS2040C", "CS2100", "CS2102")
+            getCodeSet("CS1010", "MA1301", "GER1000")
     );
 
     private static final DegreePlanner YEAR_1_SEMESTER_2 = new DegreePlanner(
             new Year("1"),
             new Semester("2"),
-            getCodeSet()
+            getCodeSet("CS1231", "CS2040C", "CS2100", "CS2107")
     );
 
     private static final DegreePlanner YEAR_1_SEMESTER_3 = new DegreePlanner(
@@ -94,13 +307,13 @@ public class SampleDataUtil {
     private static final DegreePlanner YEAR_2_SEMESTER_1 = new DegreePlanner(
             new Year("2"),
             new Semester("1"),
-            getCodeSet()
+            getCodeSet("CS2102", "CS2105", "CS2106", "CS2101", "CS2113T")
     );
 
     private static final DegreePlanner YEAR_2_SEMESTER_2 = new DegreePlanner(
             new Year("2"),
             new Semester("2"),
-            getCodeSet()
+            getCodeSet("CS3235")
     );
 
     private static final DegreePlanner YEAR_2_SEMESTER_3 = new DegreePlanner(
@@ -163,79 +376,16 @@ public class SampleDataUtil {
             getCodeSet()
     );
 
-    private static final RequirementCategory COMPUTING_FOUNDATION = new RequirementCategory(
-            new Name("Computing Foundation"), new Credits("36"),
-            getCodeSet("CS1010", "CS1231", "CS2040C", "CS2100", "CS2102")
-    );
-    private static final RequirementCategory INFORMATION_SECURITY_REQUIREMENTS = new RequirementCategory(
-            new Name("Information Security Requirements"),
-            new Credits("20"),
-            getCodeSet()
-    );
-
-    private static final RequirementCategory INFORMATION_SECURITY_ELECTIVES = new RequirementCategory(
-            new Name("Information Security Electives"),
-            new Credits("12"),
-            getCodeSet()
-    );
-
-    private static final RequirementCategory COMPUTING_BREADTH = new RequirementCategory(
-            new Name("Computing Breadth"),
-            new Credits("20"),
-            getCodeSet()
-    );
-
-    private static final RequirementCategory IT_PROFESSIONALISM = new RequirementCategory(
-            new Name("IT Professionalism"),
-            new Credits("8"),
-            getCodeSet()
-    );
-
-    private static final RequirementCategory MATHEMATICS = new RequirementCategory(
-            new Name("Mathematics"),
-            new Credits("12"),
-            getCodeSet()
-    );
-
-    private static final RequirementCategory GENERAL_EDUCATION = new RequirementCategory(
-            new Name("General Education"),
-            new Credits("20"),
-            getCodeSet()
-    );
-
-    private static final RequirementCategory UNRESTRICTED_ELECTIVES = new RequirementCategory(
-            new Name("Unrestricted Electives"),
-            new Credits("12"),
-            getCodeSet()
-    );
-
     public static Module[] getSampleModules() {
         return new Module[] {
-            CS1010, CS1231, CS2040C, CS2100, CS2102
+            CS1010, CS1231, CS2040C, CS2100, CS2101, CS2102, CS2105, CS2106, CS2107, CS2113T, CS3235, CS4238, CS5331,
+            GER1000, GEQ1000,
+            IFS4205, IS1103, IS3103, IS4231,
+            MA1301, MA1101R, MA1521, ST2334
         };
     }
 
-    public static DegreePlanner[] getSampleDegreePlanners() {
-        return new DegreePlanner[] {
-            YEAR_1_SEMESTER_1,
-            YEAR_1_SEMESTER_2,
-            YEAR_1_SEMESTER_3,
-            YEAR_1_SEMESTER_4,
-            YEAR_2_SEMESTER_1,
-            YEAR_2_SEMESTER_2,
-            YEAR_2_SEMESTER_3,
-            YEAR_2_SEMESTER_4,
-            YEAR_3_SEMESTER_1,
-            YEAR_3_SEMESTER_2,
-            YEAR_3_SEMESTER_3,
-            YEAR_3_SEMESTER_4,
-            YEAR_4_SEMESTER_1,
-            YEAR_4_SEMESTER_2,
-            YEAR_4_SEMESTER_3,
-            YEAR_4_SEMESTER_4
-        };
-    }
-
+    // Getter to retrieve all sample requirement categories
     public static RequirementCategory[] getSampleRequirementCategories() {
         return new RequirementCategory[] {
             COMPUTING_FOUNDATION,
@@ -246,6 +396,16 @@ public class SampleDataUtil {
             MATHEMATICS,
             GENERAL_EDUCATION,
             UNRESTRICTED_ELECTIVES
+        };
+    }
+
+    // Getter to retrieve all sample degree planners
+    public static DegreePlanner[] getSampleDegreePlanners() {
+        return new DegreePlanner[] {
+            YEAR_1_SEMESTER_1, YEAR_1_SEMESTER_2, YEAR_1_SEMESTER_3, YEAR_1_SEMESTER_4,
+            YEAR_2_SEMESTER_1, YEAR_2_SEMESTER_2, YEAR_2_SEMESTER_3, YEAR_2_SEMESTER_4,
+            YEAR_3_SEMESTER_1, YEAR_3_SEMESTER_2, YEAR_3_SEMESTER_3, YEAR_3_SEMESTER_4,
+            YEAR_4_SEMESTER_1, YEAR_4_SEMESTER_2, YEAR_4_SEMESTER_3, YEAR_4_SEMESTER_4
         };
     }
 
